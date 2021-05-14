033955c256ce42e54addeb5c256282f7537d8bcf
Chehui Chou <deadshot465@gmail.com>
2021-05-15 07:31:31 +0900

    Add more test cases.

--- spec/owoify_rb_spec.rb
+++ spec/owoify_rb_spec.rb
@@ -3,36 +3,44 @@
 require 'owoify_rb'
 require 'rspec'
 
+SOURCE = 'Hello World! This is the string to owo! Kinda cute, isn\'t it?'
+POKEMON_NAME_LIST_PATH = File.expand_path('./assets/pokemons.txt')
+WAR_AND_PEACE_PATH = File.expand_path('./assets/war_and_peace_chapter01-20.txt')
+POKEMON_NAME_LIST = File.readlines(POKEMON_NAME_LIST_PATH)
+WAR_AND_PEACE_TEXT = File.read(WAR_AND_PEACE_PATH)
+
 RSpec.describe 'owoify_rb' do
   it 'should not be equal to the source string' do
-    expect(Owoify.owoify('Hello World! This is the string to owo! Kinda cute, isn\'t it?',
-                         'owo')).not_to eql('Hello World! This is the string to owo! Kinda cute, isn\'t it?')
+    expect(Owoify.owoify(SOURCE, 'owo')).not_to eql(SOURCE)
   end
 
   context 'when the level is owo' do
-    let(:result) { Owoify.owoify('Hello World', 'owo') }
+    let(:result) { Owoify.owoify(SOURCE, 'owo') }
     it 'should owoify correctly' do
       expect(result).not_to eql('')
+      expect(result).not_to eql(nil)
     end
   end
 
   context 'when the level is uwu' do
-    let(:result) { Owoify.owoify('Hello World', 'uwu') }
+    let(:result) { Owoify.owoify(SOURCE, 'uwu') }
     it 'should owoify correctly' do
       expect(result).not_to eql('')
+      expect(result).not_to eql(nil)
     end
   end
 
   context 'when the level is uvu' do
-    let(:result) { Owoify.owoify('Hello World', 'uvu') }
+    let(:result) { Owoify.owoify(SOURCE, 'uvu') }
     it 'should owoify correctly' do
       expect(result).not_to eql('')
+      expect(result).not_to eql(nil)
     end
   end
 
   context 'when the level is undefined' do
     it 'should not owoify correctly' do
-      expect { Owoify.owoify('Hello World', '123') }.to raise_error(ArgumentError)
+      expect { Owoify.owoify(SOURCE, '123') }.to raise_error(ArgumentError)
     end
   end
 
@@ -52,5 +60,31 @@ RSpec.describe 'owoify_rb' do
     it 'should not be equal' do
       expect(owo).not_to eql(uvu)
     end
+  end
+
+  it 'should owoify pokemon names' do
+    POKEMON_NAME_LIST.each do |name|
+      name_with_owo = Owoify.owoify(name)
+      name_with_uwu = Owoify.owoify(name, 'uwu')
+      name_with_uvu = Owoify.owoify(name, 'uvu')
+      expect(name_with_owo).not_to eql('')
+      expect(name_with_uwu).not_to eql('')
+      expect(name_with_uvu).not_to eql('')
+      expect(name_with_owo).not_to eql(nil)
+      expect(name_with_uwu).not_to eql(nil)
+      expect(name_with_uvu).not_to eql(nil)
+    end
+  end
+
+  it 'should owoify long text' do
+    text_with_owo = Owoify.owoify(WAR_AND_PEACE_TEXT)
+    text_with_uwu = Owoify.owoify(WAR_AND_PEACE_TEXT, 'uwu')
+    text_with_uvu = Owoify.owoify(WAR_AND_PEACE_TEXT, 'uvu')
+    expect(text_with_owo).not_to eql('')
+    expect(text_with_uwu).not_to eql('')
+    expect(text_with_uvu).not_to eql('')
+    expect(text_with_owo).not_to eql(nil)
+    expect(text_with_uwu).not_to eql(nil)
+    expect(text_with_uvu).not_to eql(nil)
   end
 end
